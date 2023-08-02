3124d0cc48e1f4a16d4b2fcbeefd6b023ff65079
MuhammadTalhaKhan007 <talhakhan@arcturuslogic.com>
2023-08-02 17:24:01 +0500

    Ok

--- dbconfig/dbconfig.ts
+++ dbconfig/dbconfig.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export async function connect() {
     try {
-        mongoose.connect('mongodb+srv://doadmin:h8DC0WgL254M61p3@db-mongodb-nyc1-08654-eea55fda.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-nyc1-08654&tls=true&directConnection=true');
+        mongoose.connect('mongodb+srv://doadmin:h8DC0WgL254M61p3@db-mongodb-nyc1-08654-eea55fda.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-nyc1-08654&tls=true');
         const connection = mongoose.connection;
         connection.on('connected', () => {
             console.log('mongoDB connection is created!!! ');
