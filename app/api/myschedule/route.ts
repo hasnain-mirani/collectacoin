cf51a363213d695017bb8be910b4b0a71248b764
MuhammadTalhaKhan007 <talhakhan@arcturuslogic.com>
2023-08-02 16:37:45 +0500

    !

--- app/api/myschedule/route.ts
+++ app/api/myschedule/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { connect } from "@/dbconfig/dbconfig";
-import event from "@/modals/eventModal";
+import event from "@/modals/eventModal"; 
 
 connect();
 
-export async function GET(){
+export async function GET() { 
   const events = await event.find();
-  return NextResponse.json({events});
+  return NextResponse.json({ events });
 }
