717feb4b9a958f90651dd2b32b0025b9493cb8c1
MuhammadTalhaKhan007 <139450184+MuhammadTalhaKhan007@users.noreply.github.com>
2023-08-14 14:46:15 +0500

    Update and rename dbconfig.ts to dbconfig.js

---
+++ dbconfig/dbconfig.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+export async function connect() {
+    try {
+        await mongoose.connect('mongodb+srv://doadmin:346eoR1k27Ttu58d@db-mongodb-nyc1-08654-eea55fda.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-08654', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log('MongoDB connection is established!');
+        });
+
+        connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+            // You can decide whether to gracefully handle the error or not
+        });
+
+        connection.on('disconnected', () => {
+            console.log('MongoDB connection lost. Reconnecting...');
+        });
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
