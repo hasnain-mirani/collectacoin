126a7e7cc1f0396a5c23d5da92bfb9c295c44e41
MuhammadTalhaKhan007 <139450184+MuhammadTalhaKhan007@users.noreply.github.com>
2023-08-15 11:49:40 +0500

    Update

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
 
-  const isPublicPath = path === '/signin' || path === '/signup' || path === '/verifyemail'
+  const isPublicPath = path === '/signin' || path === '/' || path === '/verifyemail'
 
   const token = request.cookies.get('token')?.value || ''
 
