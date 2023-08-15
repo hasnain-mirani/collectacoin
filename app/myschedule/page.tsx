1263e04b1fd946ee1259839a49811e5288742d79
MuhammadTalhaKhan007 <139450184+MuhammadTalhaKhan007@users.noreply.github.com>
2023-08-15 15:41:31 +0500

    Ok

--- app/myschedule/page.tsx
+++ app/myschedule/page.tsx
@@ -56,7 +56,6 @@ export default function MySchedule(){
   // }
 
   const [activePage, setActivePage] = useState<string>("home");
-  console.log(pathname);
   useEffect(() => {
     let paths = pathname.split("/");
 
