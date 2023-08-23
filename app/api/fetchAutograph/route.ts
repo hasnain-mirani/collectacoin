a9fb372ecacf64a3c9652cd5427e542c27a04317
MuhammadTalhaKhan007 <139450184+MuhammadTalhaKhan007@users.noreply.github.com>
2023-08-23 13:08:55 +0500

    Update route.ts

--- app/api/fetchAutograph/route.ts
+++ app/api/fetchAutograph/route.ts
@@ -3,7 +3,7 @@ import createEvent from "@/modals/createEventModal";
 import { NextResponse } from "next/server";
 
 connect();
-
+export const revalidate = 0;
 export async function GET() {
   try {
     const allAutographs = await createEvent.find({ eventType: "Autograph" });
