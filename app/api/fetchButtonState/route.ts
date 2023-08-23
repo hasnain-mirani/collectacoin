826cc3ca3a2bd6bc1bbd830a5fbc817ac4dc94ff
MuhammadTalhaKhan007 <139450184+MuhammadTalhaKhan007@users.noreply.github.com>
2023-08-23 13:08:48 +0500

    Update route.ts

--- app/api/fetchButtonState/route.ts
+++ app/api/fetchButtonState/route.ts
@@ -3,7 +3,7 @@ import {connect} from "@/dbconfig/dbconfig";
 import buttonEvent from "@/modals/buttonModal";
 
 connect();
-
+export const revalidate = 0;
 export async function GET(){
     try {
         const buttonStates = await buttonEvent.find().sort({buttonNumber: 1});
